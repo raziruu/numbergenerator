a776167f082d6c4660c0edacb93f3a84d46e6e22
raziruu <shrestharajill11@gmail.com>
2024-05-20 10:15:58 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/rajil/numbergenerator/numbergenerator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rajil.numbergenerator.numbergenerator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/number_generator_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NumberGenerator extends StatefulWidget {
+  const NumberGenerator({super.key});
+
+  @override
+  State<NumberGenerator> createState() => _NumberGeneratorState();
+}
+
+class _NumberGeneratorState extends State<NumberGenerator> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
